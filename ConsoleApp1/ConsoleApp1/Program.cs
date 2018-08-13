fb7884c743db24c0936e7902c2d274741ae24851
FreshAloe <42317511+FreshAloe@users.noreply.github.com>
2018-08-14 01:23:42 +0900

    180813

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,22 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Wrold!");
+            Book gulliver = new Book();
+
+            gulliver.Title = "걸리버 여행기";
+            gulliver.Author = "Jonathan Swift";
+            gulliver.ISBN13 = 9788983920775m;
+            gulliver.Contents = "...";
+            gulliver.PageCount = 384;
         }
+    }
+
+    class Book
+    {
+        public string Title;
+        public decimal ISBN13;
+        public string Contents;
+        public string Author;
+        public int PageCount;
     }
 }
