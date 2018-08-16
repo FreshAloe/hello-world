83c3c8fd8d2306cad907f021742035ac23ff0a5c
FreshAloe <42317511+FreshAloe@users.noreply.github.com>
2018-08-17 00:50:58 +0900

    180816

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,33 +10,92 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Kilogram k1 = new Kilogram(5);
-            Kilogram k2 = new Kilogram(10);
+            Person[] peoples = new Person[]
+            {
+                new Person(51, "Anders"),
+                new Person(37, "Scott"),
+                new Person(45, "Peter"),
+                new Person(62, "Mads"),
+                new Person(28, "James")
+            };
 
-            Kilogram k3 = k1 + k2;
-            Console.WriteLine(k3);
+            SortPerson sortPerson = new SortPerson(peoples);
+            sortPerson.Sort(AscSortByName);
+            sortPerson.Display();
+
+            Console.WriteLine();
+
+            sortPerson.Sort(AscSortByAge);
+            sortPerson.Display();
         }
 
+        static bool AscSortByName(Person men1, Person men2)
+        {
+            return men1.Name.CompareTo(men2.Name) < 0;
+        }
+
+        static bool AscSortByAge(Person men1, Person men2)
+        {
+            return men1.Age < men2.Age;
+        }
     }
 
-    class Kilogram
+    class Person
     {
-        double mass;
+        public int Age;
+        public string Name;
 
-        public Kilogram(double value)
+        public Person(int age, string name)
         {
-            this.mass = value;
-        }
-
-        public static Kilogram operator +(Kilogram k1, Kilogram k2)
-        {
-            return new Kilogram(k1.mass + k2.mass);
+            this.Age = age;
+            this.Name = name;
         }
 
         public override string ToString()
         {
-            return this.mass.ToString() + " kg";
+            return Name + ": " + Age;
         }
     }
 
+    delegate bool CompareDelegate(Person men1, Person men2);
+
+    class SortPerson
+    {
+        Person[] men;
+
+        public SortPerson(Person[] men)
+        {
+            this.men = men;
+        }
+
+        public void Sort(CompareDelegate compareMethod)
+        {
+            Person temp;
+
+            for(int i = 0; i < men.Length; i++)
+            {
+                int lowPos = i;
+
+                for(int j = i + 1; j < men.Length; j++)
+                {
+                    if(compareMethod(men[j], men[lowPos]))
+                    {
+                        lowPos = j;
+                    }
+                }
+
+                temp = men[lowPos];
+                men[lowPos] = men[i];
+                men[i] = temp;
+            }
+        }
+
+        public void Display()
+        {
+            foreach(Person item in men)
+            {
+                Console.WriteLine(item + ", ");
+            }
+        }
+    }
 }
