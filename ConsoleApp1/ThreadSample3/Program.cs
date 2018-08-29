df1ce2ff07c17528221eda5323a600afede65815
FreshAloe <42317511+FreshAloe@users.noreply.github.com>
2018-08-30 01:44:58 +0900

    180829_1

---
+++ ConsoleApp1/ThreadSample3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadSample3
+{
+    class Program
+    {
+        int number = 0;
+
+        static void Main(string[] args)
+        {
+            Program pg = new Program();
+
+            Thread thread1 = new Thread(ThreadFunc);
+            Thread thread2 = new Thread(ThreadFunc);
+
+            thread1.Start(pg);
+            thread2.Start(pg);
+
+            thread1.Join();
+            thread2.Join();
+
+            Console.WriteLine(pg.number);
+        }
+
+        static void ThreadFunc(object obj)
+        {
+            Program pg = obj as Program;
+
+            for (int i = 0; i < 10000; i++)
+            {
+                /*
+                Monitor.Enter(pg);
+                try
+                {
+                    pg.number++;
+                }
+                finally
+                {
+                    Monitor.Exit(pg);
+                }*/
+
+                lock (pg)
+                {
+                    pg.number++;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ThreadSample4/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadSample4
+{
+    class MyData
+    {
+        int number = 0;
+        public object _numberlock = new object();
+
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+        }
+
+        public void Increment()
+        {
+            lock(_numberlock)
+            {
+                number++;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyData data = new MyData();
+
+            Thread thread1 = new Thread(ThreadFunc);
+            Thread thread2 = new Thread(ThreadFunc);
+
+            thread1.Start(data);
+            thread2.Start(data);
+
+            thread1.Join();
+            thread2.Join();
+
+            Console.WriteLine(data.Number);
+        }
+
+        static void ThreadFunc(object obj)
+        {
+            MyData data = obj as MyData;
+
+            for (int i = 0; i < 100000; i++)
+            {
+                lock (data)
+                {
+                    data.Increment();
+                }
+            }
+        }
+    }
+}
