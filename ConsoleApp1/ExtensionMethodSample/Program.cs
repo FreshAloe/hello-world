658bde90f6274cc2f271bef9559b9ecf6e0508b1
FreshAloe <capgmal@gmail.com>
2018-09-11 16:08:41 +0900

    180911_1

---
+++ ConsoleApp1/ExtensionMethodSample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Hello World";
+            Console.WriteLine(text.GetWordCount());
+        }
+    }
+
+    static class ExtensionMethod
+    {
+        public static int GetWordCount(this string txt)
+        {
+            return txt.Split(' ').Length;
+        }
+    }
+}
